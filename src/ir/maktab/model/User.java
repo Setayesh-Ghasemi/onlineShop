0bc5a38adfeb981bb6b0dab4f806c9baa75dc5cb
Setayesh Ghasemi <setayesh ghasemi>
2021-10-18 12:29:01 +0330

    add user property

---
+++ src/ir/maktab/model/User.java
@@ -0,0 +1,14 @@
+package ir.maktab.model;
+
+import java.sql.Date;
+
+public class User {
+
+    private int id;
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private String nationalId;
+    private Date birthDate;
+    
+}
